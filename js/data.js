2c209582b21fe70f9ba179d5bca5985b7d946fae
leonardokcp12395-cyber <leonardokcp12395@gmail.com>
2025-08-23 09:50:37 -0300

    Delete js directory

--- js/data.js
+++
@@ -1,7 +0,0 @@
-import * as state from './state.js';
-
-export function savePermanentData() {
-    localStorage.setItem('playerGems', state.playerGems);
-    localStorage.setItem('playerUpgrades', JSON.stringify(state.playerUpgrades));
-    localStorage.setItem('playerAchievements', JSON.stringify(state.playerAchievements));
-}
--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { setGameState } from './ui.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM fully loaded and parsed. Initializing game...");
-    setGameState('menu');
-    console.log("Game state set to menu.");
-});
